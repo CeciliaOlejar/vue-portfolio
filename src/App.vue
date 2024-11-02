ce3c72e918c1945c71f9eb9e3789ec789c11dbde
Cecilia Olejarczyk <chechuolejarczyk@gmail.com>
2024-11-02 18:40:07 -0300

    clase 3

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,26 @@
 </script>
 
 <template>
-  <h1>hola</h1>
+  <header>
+    <!-- nav bar -->
+    <h1>hola mundillo</h1>
+    <!-- datos personales -->
+  </header>
+  <main>
+    <!-- secciones -->
+  </main>
+  <footer>
+    <a href="#top">Inicio</a>
+    <p> Portafolio web - tutorial UTN FRSR</p>
+  </footer>
 </template>
 
 <style scoped>
-
+footer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2rem;
+  font-size: 1.3rem;
+}
 </style>
