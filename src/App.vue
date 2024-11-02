79dd66657f4086194b53d48bc47c567a066669f9
Cecilia Olejarczyk <chechuolejarczyk@gmail.com>
2024-11-02 19:24:13 -0300

    correccion

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-import Experiencia from './components/Experiencia.vue';
-import Habilidades from './components/Habilidades.vue';
-import Intereses from './components/Intereses.vue';
+import DatosPersonales from './components/DatosPersonales.vue';
+// import Educacion from './components/Educacion.vue';
+// import Experiencia from './components/Experiencia.vue';
+// import Habilidades from './components/Habilidades.vue';
+// import Intereses from './components/Intereses.vue';
 import navBar from './components/navBar.vue';
-import Proyectos from './components/Proyectos.vue';
+// import Proyectos from './components/Proyectos.vue';
 </script>
 
 <template>
@@ -12,34 +14,33 @@ import Proyectos from './components/Proyectos.vue';
     <nav-bar />
     <h1>hola mundillo</h1>
     <!-- datos personales -->
+     <DatosPersonales/>
+     
   </header>
   <main>
-    <!-- secciones -->
-    <!--el estilo css de section se encuentra en base.css-->
     <section id="educacion">
       <h2>Educación - Cursos</h2>
-      <!--Componente Educación-->
       <Educacion />
     </section>
+
     <section id="experiencia">
       <h2>Experiencia</h2>
-      <!--Componente Experiencia-->
       <Experiencia />
     </section>
+
     <section id="proyectos">
       <h2>Proyectos</h2>
-      <!--Componente Proyectos-->
-      <Proyectos/>
+      <Proyectos />
     </section>
+
     <section id="habilidades">
-      <!--Componente Habilidades-->
       <h2>Habilidades</h2>
       <Habilidades />
     </section>
+
     <section id="intereses">
-      <!--Componente Intereses-->
       <h2>Intereses</h2>
-      <Intereses />
+      <!-- <Intereses /> -->
     </section>
   </main>
   <footer>
