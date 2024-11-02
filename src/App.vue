ec8243dd02286afc4d7690443f97fe9ec85f2e31
Cecilia Olejarczyk <chechuolejarczyk@gmail.com>
2024-11-02 19:15:59 -0300

    clase4

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <script setup>
+import Experiencia from './components/Experiencia.vue';
+import Habilidades from './components/Habilidades.vue';
+import Intereses from './components/Intereses.vue';
 import navBar from './components/navBar.vue';
+import Proyectos from './components/Proyectos.vue';
 </script>
 
 <template>
@@ -11,6 +15,32 @@ import navBar from './components/navBar.vue';
   </header>
   <main>
     <!-- secciones -->
+    <!--el estilo css de section se encuentra en base.css-->
+    <section id="educacion">
+      <h2>Educación - Cursos</h2>
+      <!--Componente Educación-->
+      <Educacion />
+    </section>
+    <section id="experiencia">
+      <h2>Experiencia</h2>
+      <!--Componente Experiencia-->
+      <Experiencia />
+    </section>
+    <section id="proyectos">
+      <h2>Proyectos</h2>
+      <!--Componente Proyectos-->
+      <Proyectos/>
+    </section>
+    <section id="habilidades">
+      <!--Componente Habilidades-->
+      <h2>Habilidades</h2>
+      <Habilidades />
+    </section>
+    <section id="intereses">
+      <!--Componente Intereses-->
+      <h2>Intereses</h2>
+      <Intereses />
+    </section>
   </main>
   <footer>
     <a href="#top">Inicio</a>
---
+++ src/components/DatosPersonales.vue
@@ -0,0 +1,56 @@
+<script setup>
+
+</script>
+
+<template>
+    <section class="datos-personales">
+        <div class="card">
+            <h1>Cecilia Olejar</h1>
+            <ul class="container-lista">
+                <li><a href=""><img class="icon-redsocial" src="" width="35rem" alt="">lindkin</a></li>
+                <li><a href=""><img class="icon-redsocial" src="" width="35rem" alt="">insta</a></li>
+                <li><a href=""><img class="icon-redsocial" src="" width="35rem" alt="">git</a></li>
+                <li><a href=""><img class="icon-redsocial" src="" width="35rem" alt="">cv</a></li>
+            </ul>
+        </div>
+    </section>
+</template>
+
+
+
+<style>
+h1 {
+    font-size: 2.5rem;
+}
+
+.card {
+    background-color: rgb(28, 41, 52);
+    border-radius: 10px;
+    padding: 10px;
+    margin: 10px;
+    text-align: center;
+}
+
+.container-lista {
+    display: flex;
+    justify-content: center;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    text-align: center;
+}
+
+.icon-redsocial {
+    align-items: center;
+    background-color: aliceblue;
+    border-radius: 50%;
+    padding: 2px;
+    margin: 5px;
+    box-shadow: 0 0 5px rgba(95, 124, 205, 0.934);
+}
+
+.icon-redsocial:hover {
+    background-color: rgb(28, 41, 52);
+    box-shadow: 0 0 5px rgba(251, 249, 249, 0.934);
+}
+</style>
